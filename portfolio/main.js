b8de30c6f917388c8e8010266a0e7c7747cc4e38
jayson7 <lexxiijoo70@gmail.com>
2022-03-26 11:59:27 +0100

    section1 and nav

--- portfolio/main.js
+++ portfolio/main.js
@@ -8,3 +8,9 @@ function showPage() {
     document.getElementById("loader").style.display = "none";
     document.getElementById("myDiv").style.display = "block";
 }
+let toggle = document.querySelector(".toggle");
+navbar = document.querySelector("nav");
+toggle.addEventListener("click", () => {
+    navbar.classList.toggle("active");
+
+});
