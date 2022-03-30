0c17131668797c9d7b252ea0cfbcddeaf69d6bdd
jayson7 <lexxiijoo70@gmail.com>
2022-03-30 13:55:55 +0100

    sec 4 % sec 5

--- portfolio/main.js
+++ portfolio/main.js
@@ -12,5 +12,26 @@ let toggle = document.querySelector(".toggle");
 navbar = document.querySelector("nav");
 toggle.addEventListener("click", () => {
     navbar.classList.toggle("active");
-
 });
+
+// scrollspy section
+let section = document.querySelectorAll("section");
+let navlinks = document.querySelectorAll("nav ul li a");
+
+window.onscroll = () => {
+    section.forEach((sec) => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop;
+        let height = sec.offsetHeight;
+        let classess = sec.getAttribute("id");
+
+        if (top >= offset && top < offset + height) {
+            navlinks.forEach((links) => {
+                links.classList.remove("active-nav");
+                document
+                    .querySelector("nav ul li a[href*=" + classess + "]")
+                    .classList.add("active-nav");
+            });
+        }
+    });
+};
